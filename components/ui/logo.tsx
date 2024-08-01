4caff40634998e0434f4818a699edd160635788e
dherring8 <dherring8@gatech.edu>
2024-08-01 17:26:45 -0500

    fix

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex" aria-label="Cruip">
-      <img src="/images/ReferMe_logo.png" alt="Logo" width="28" height="28" />
+      <img src="/public/images/ReferMe_logo.png" alt="Logo" width="28" height="28" />
     </Link>
   );
 }
