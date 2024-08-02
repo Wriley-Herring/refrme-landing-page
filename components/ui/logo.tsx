c4eb5f8f7075475240bd806b512cf965ed1f55a3
dherring8 <dherring8@gatech.edu>
2024-08-02 10:22:54 -0500

    fix

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import RefrmeLogo from "@/public/images/ReferMe_full.png"; // Adjust the path as needed
+import RefrmeLogo from "@/public/images/ReferMe_logo.png"; // Adjust the path as needed
 
 export default function Logo() {
   return (
@@ -8,8 +8,8 @@ export default function Logo() {
        <Image
                 className="box-content"
                 src={RefrmeLogo}
-                width={150}
-                height={150}
+                width={40}
+                height={40}
                 alt="Refrme Logo"
               />
     </Link>
