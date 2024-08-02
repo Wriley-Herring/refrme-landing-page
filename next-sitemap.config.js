cc49f6accfe7297b9325d13868b0ea88bd607e80
dherring8 <dherring8@gatech.edu>
2024-08-02 15:56:33 -0500

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.refrme.io', // Change to your website URL
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  };
+  
