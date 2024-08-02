685dc75b55cf92ab4fb784807bc6ec328b8a2ce6
dherring8 <dherring8@gatech.edu>
2024-08-01 19:34:48 -0500

    fix

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
+import RefrmeLogo from "@/public/images/ReferMe_logo.png"; // Adjust the path as needed
 
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex" aria-label="Cruip">
-      <img src="/public/images/ReferMe_logo.png" alt="Logo" width="28" height="28" />
+       <Image
+                className="box-content"
+                src={RefrmeLogo}
+                width={28}
+                height={28}
+                alt="Refrme Logo"
+              />
     </Link>
   );
 }
